2d6e07e14b0ffdbd195324f42d8879142191a418
Matías Lang <yo@matiaslang.me>
2019-04-18 19:33:29 -0300

    Revert "Fix colorscheme fox 6.2"
    This reverts commit 129df979f9eb51ca8c21d5ab4756927392b1948f.

--- config.def.h
+++ config.def.h
@@ -2,6 +2,12 @@
 
 /* appearance */
 
+static const char normbordercolor[] = "#444444";
+static const char normbgcolor[]     = "#002b36";
+static const char normfgcolor[]     = "#bbbbbb";
+static const char selbordercolor[]  = "#f4ad42";
+static const char selbgcolor[]      = "#005577";
+static const char selfgcolor[]      = "#eeeeee";
 static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int gappx     = 18;       /* gap pixel between windows */
 
@@ -10,16 +16,15 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "monospace:size=10" };
 static const char dmenufont[]       = "monospace:size=10";
-static const char col_gray1[]       = "#002b36";
+static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#f4ad42";
-static const char selbordercolor[]  = "#f4ad42";
+static const char col_cyan[]        = "#005577";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  selbordercolor  },
+	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
 };
 
 /* tagging */
