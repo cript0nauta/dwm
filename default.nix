76a4169ca111d9252c03ba279f12b0bc24a79ac6
Matías Lang <yo@matiaslang.me>
2019-06-08 16:47:59 -0300

    Use debug binary

--- default.nix
+++ default.nix
@@ -1,6 +1,8 @@
 with import <nixpkgs> {};
 
 dwm.overrideAttrs (oldAttrs: rec {
+  dontPatchELF = true;
+  dontStrip = true;
   src = builtins.filterSource
     (path: type:
     baseNameOf path != ".git" &&
